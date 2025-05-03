d8326c20aac177c654d2c9c39986ca53e25170ba
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-05-03 12:42:13 +0100

    try/catch for openai sdk

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import OpenAI, AuthenticationError, APIError
 load_dotenv()
 import os
 API_KEY = os.getenv("API_KEY")
@@ -8,14 +8,22 @@ API_BASE_URL = os.getenv("API_BASE_URL")
 print(API_KEY)
 print(API_BASE_URL)
 
-client = OpenAI(api_key=API_KEY, base_url=API_BASE_URL)
-response = client.chat.completions.create(
-  model="deepseek-chat",
-  messages=[
-    {"role":"system", "content":"you are a helpful assistant"},
-    {"role":"user", "content":"hello"}
-  ],
-  stream=False
-)
+try:
+  client = OpenAI(api_key=API_KEY, base_url=API_BASE_URL)
+  response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+      {"role":"system", "content":"you are a helpful assistant"},
+      {"role":"user", "content":"hello"}
+    ],
+    stream=False
+  )
+except AuthenticationError as auth_e:
+  print(auth_e)
+except APIError as api_e:
+  print(api_e)
+except Exception as e:
+  print(e)
+
 
 print(response.choices[0].message.content)
